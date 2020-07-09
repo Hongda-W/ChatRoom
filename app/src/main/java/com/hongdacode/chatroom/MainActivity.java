12a69cdfddbb2db77a3ac44c52089100cbb0ddfb
Hongda-W <hongda.wang@colorado.edu>
2020-07-08 19:05:56 -0600

    Finished login and register activity design and function implementation. Add dropdown in main activity to allow logging out.

--- app/src/main/java/com/hongdacode/chatroom/MainActivity.java
+++ app/src/main/java/com/hongdacode/chatroom/MainActivity.java
@@ -2,13 +2,17 @@ package com.hongdacode.chatroom;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
@@ -20,11 +24,15 @@ public class MainActivity extends AppCompatActivity {
     private TabAdaptor mTabAdaptor;
 
     private FirebaseUser user;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
 
         mToolbar = findViewById(R.id.main_bar);
         setSupportActionBar(mToolbar);
@@ -52,5 +60,30 @@ public class MainActivity extends AppCompatActivity {
     private void sendToLoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.menu_options, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.menu_logout) {
+            mAuth.signOut();
+            sendToLoginActivity();
+        }
+        else if (item.getItemId() == R.id.menu_settings) {
+
+        }
+        else if (item.getItemId() == R.id.menu_add_someone) {
+        }
+        return true;
     }
 }
