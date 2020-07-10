71116eef75c791759b06d34a8b5b67a5603d40f1
Hongda-W <hongda.wang@colorado.edu>
2020-07-10 15:03:11 -0600

    Send and cancel friend request.

--- app/src/main/java/com/hongdacode/chatroom/UserProfileActivity.java
+++ app/src/main/java/com/hongdacode/chatroom/UserProfileActivity.java
@@ -3,6 +3,7 @@ package com.hongdacode.chatroom;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +11,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,21 +26,24 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserProfileActivity extends AppCompatActivity {
 
-    private String userID;
+    private String userID, myUserID, requestStatus;
     private CircleImageView mImageView;
     private TextView mUsername, mUserBio;
     private Button mSendRequestButton;
 
     private DatabaseReference mDatabaseRef;
-    private FirebaseStorage mImageRef;
+    private FirebaseAuth mAuth;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
         userID = getIntent().getExtras().get("userID").toString();
+
+        mAuth = FirebaseAuth.getInstance();
+        myUserID = mAuth.getCurrentUser().getUid();
 
         loadViews();
 
@@ -56,6 +63,33 @@ public class UserProfileActivity extends AppCompatActivity {
                             mUsername.setText(outUserName);
                             mUserBio.setText(outUserBio);
                         }
+                        if ( myUserID.equals(userID) ){
+                            mSendRequestButton.setVisibility(View.INVISIBLE); // hide send friend request button if clicked on self
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        mDatabaseRef.child("FriendRequest").child(myUserID)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()){
+                            if ( snapshot.hasChild(userID)){
+                                requestStatus = snapshot.child(userID).child("request").getValue().toString();
+                                if (requestStatus.equals("sent")){
+                                    mSendRequestButton.setText("Cancel friend request");
+                                } else if (requestStatus.equals("cancelled")){
+                                    mSendRequestButton.setText("Send request");
+                                }
+                            } else {
+                                requestStatus = "no request";
+                            }
+                        }
                     }
 
                     @Override
@@ -67,13 +101,38 @@ public class UserProfileActivity extends AppCompatActivity {
         mSendRequestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendFriendRequest(userID);
+                sendFriendRequest();
             }
         });
     }
 
-    private void sendFriendRequest(String identity) {
-        Toast.makeText(UserProfileActivity.this, "send friend request to " + identity, Toast.LENGTH_LONG).show();
+    private void sendFriendRequest() {
+        if (requestStatus.equals("no request") || requestStatus.equals("cancelled")){
+            mDatabaseRef.child("FriendRequest").child(myUserID).child(userID).child("request")
+                    .setValue("sent")
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(UserProfileActivity.this, "Friend request sent.", Toast.LENGTH_SHORT).show();
+                                sendToMainActivity();
+                            }
+                        }
+                    });
+        }
+        if (requestStatus.equals("sent")){
+            mDatabaseRef.child("FriendRequest").child(myUserID).child(userID).child("request")
+                    .setValue("cancelled")
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(UserProfileActivity.this, "Friend request cancelled.", Toast.LENGTH_SHORT).show();
+                                sendToMainActivity();
+                            }
+                        }
+                    });
+        }
     }
 
     private void loadViews() {
@@ -81,5 +140,12 @@ public class UserProfileActivity extends AppCompatActivity {
         mUsername = findViewById(R.id.friend_profile_username);
         mUserBio = findViewById(R.id.friend_profile_bio);
         mSendRequestButton = findViewById(R.id.send_friend_request);
+    }
+
+    private void sendToMainActivity() {
+        Intent mainIntent = new Intent(UserProfileActivity.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 }
