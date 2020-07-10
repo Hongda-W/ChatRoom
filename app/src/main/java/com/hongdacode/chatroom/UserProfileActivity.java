98df0f78d8ffd69b5ca240822610371e113ad387
Hongda-W <hongda.wang@colorado.edu>
2020-07-10 14:02:37 -0600

    Implement add friend activity.

--- app/src/main/java/com/hongdacode/chatroom/UserProfileActivity.java
+++ app/src/main/java/com/hongdacode/chatroom/UserProfileActivity.java
@@ -1,14 +1,34 @@
 package com.hongdacode.chatroom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     private String userID;
+    private CircleImageView mImageView;
+    private TextView mUsername, mUserBio;
+    private Button mSendRequestButton;
+
+    private DatabaseReference mDatabaseRef;
+    private FirebaseStorage mImageRef;
 
 
     @Override
@@ -18,6 +38,47 @@ public class UserProfileActivity extends AppCompatActivity {
 
         userID = getIntent().getExtras().get("userID").toString();
 
-        Log.d("ChartRoom", userID);
+        loadViews();
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        mDatabaseRef.child("Users").child(userID)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String outUserName = snapshot.child("Username").getValue().toString();
+                        String outUserBio = snapshot.child("UserBio").getValue().toString();
+                        if ( snapshot.hasChild("profileImage") ){
+                            String userImageURL = snapshot.child("profileImage").getValue().toString();
+                            Picasso.get().load(userImageURL).into(mImageView);
+                        }
+
+                        mUsername.setText(outUserName);
+                        mUserBio.setText(outUserBio);
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        mSendRequestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendFriendRequest(userID);
+            }
+        });
+    }
+
+    private void sendFriendRequest(String identity) {
+        Toast.makeText(UserProfileActivity.this, "send friend request to " + identity, Toast.LENGTH_LONG).show();
+    }
+
+    private void loadViews() {
+        mImageView = findViewById(R.id.friend_profile_image);
+        mUsername = findViewById(R.id.friend_profile_username);
+        mUserBio = findViewById(R.id.friend_profile_bio);
+        mSendRequestButton = findViewById(R.id.send_friend_request);
     }
 }
